9bbc34310051fb4f1a4681e9637a1a192eb8d97e
gleblevchyshyn <hlib.levchyshyn@nure.ua>
2023-04-15 20:11:48 +0300

    Implemented pipeline class that saves parsed items in json file.

--- shop_crawler/shop_crawler/pipelines.py
+++ shop_crawler/shop_crawler/pipelines.py
@@ -1,3 +1,5 @@
+import json
+from itemadapter import ItemAdapter
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
@@ -9,5 +11,16 @@ from itemadapter import ItemAdapter
 
 
 class ShopCrawlerPipeline:
+    def __init__(self):
+        self.my_json_file = None
+
+    def open_spider(self, spider):
+        self.my_json_file = open('output.jsonl', 'w')
+
+    def close_spider(self, spider):
+        self.my_json_file.close()
+
     def process_item(self, item, spider):
+        line = json.dumps(ItemAdapter(item).asdict()) + "\n"
+        self.my_json_file.write(line)
         return item
